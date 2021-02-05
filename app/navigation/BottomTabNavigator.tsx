55df5caadd3ba68b303c63f21132a83b22461c78
Ashwin-MJ <ashwinmj@gmail.com>
2021-02-05 19:50:05 +0000

    Update screen names

--- app/navigation/BottomTabNavigator.tsx
+++ app/navigation/BottomTabNavigator.tsx
@@ -1,13 +1,17 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import HiyaMainScreen from "../screens/HiyaMainScreen";
+import FriendsScreenScreen from "../screens/FriendsScreen";
+import {
+  BottomTabParamList,
+  HiyaMainParamList,
+  FriendsScreenParamList,
+} from "../types";
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -16,20 +20,25 @@ export default function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+      initialRouteName="HiyaMain"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
+        name="HiyaMain"
+        component={HiyaMainNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          ),
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
+        name="FriendsScreen"
+        component={FriendsScreenNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          ),
         }}
       />
     </BottomTab.Navigator>
@@ -38,36 +47,39 @@ export default function BottomTabNavigator() {
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof Ionicons>["name"];
+  color: string;
+}) {
   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator<TabOneParamList>();
+const HiyaMainStack = createStackNavigator<HiyaMainParamList>();
 
-function TabOneNavigator() {
+function HiyaMainNavigator() {
   return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+    <HiyaMainStack.Navigator>
+      <HiyaMainStack.Screen
+        name="HiyaMainScreen"
+        component={HiyaMainScreen}
+        options={{ headerTitle: "Hiya Main Screen" }}
       />
-    </TabOneStack.Navigator>
+    </HiyaMainStack.Navigator>
   );
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const FriendsScreenStack = createStackNavigator<FriendsScreenParamList>();
 
-function TabTwoNavigator() {
+function FriendsScreenNavigator() {
   return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
+    <FriendsScreenStack.Navigator>
+      <FriendsScreenStack.Screen
+        name="FriendsScreenScreen"
+        component={FriendsScreenScreen}
+        options={{ headerTitle: "My Friends" }}
       />
-    </TabTwoStack.Navigator>
+    </FriendsScreenStack.Navigator>
   );
 }
