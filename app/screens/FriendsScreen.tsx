53eb41edb93372c3ac598550e24af681c76dc007
Nathan Moses <nathanmoses13@gmail.com>
2021-02-05 22:45:22 +0000

    added button and accessed api

--- app/screens/FriendsScreen.tsx
+++ app/screens/FriendsScreen.tsx
@@ -1,19 +1,31 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
+function getFriends(){
+    // GET request using fetch with set headers
+    const dict = { 'Content-Type': 'application/json', 'userid' : '9UD2ZWJT' };
+    fetch('https://us-central1-hiya-b2b7f.cloudfunctions.net/getFriends ', {method: "GET", headers : dict })
+        .then((response) => {
+        response.json();
+      })
+      .then((responseJson) => {
+        console.log(responseJson);
+      })
+      
+      
+}
 export default function FriendsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Friends Screen</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/FriendsScreen.tsx" />
+      
+	  <TouchableOpacity
+         onPress={() => getFriends()}
+		 style={styles.button}>
+         <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -33,4 +45,18 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+   button: {
+	right: 125,
+	bottom: 150,
+	height: 100,
+	width: 100,
+	borderRadius: 25,
+	backgroundColor: "blue"
+	,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+	textAlign: "center",
+  }, 
 });
