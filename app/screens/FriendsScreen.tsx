dbebc2c237243e042f63cbac0378f8d53124df1c
Nathan Moses <nathanmoses13@gmail.com>
2021-02-06 18:56:22 +0000

    just really confused

--- app/screens/FriendsScreen.tsx
+++ app/screens/FriendsScreen.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
 
+import { StyleSheet, TouchableOpacity } from "react-native";
+import React,{useState,useEffect} from 'react';
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
-import { addFriend, ensureValidId, getFriends } from "../api/http_requests";
+import { addFriend, ensureValidId } from "../api/http_requests";
 
-
+const endpoint = "https://us-central1-hiya-b2b7f.cloudfunctions.net";
 
 interface friendsObj {
 	friends: string[];
@@ -13,54 +13,58 @@ interface friendsObj {
 //capture current userid
 let primaryUser: string = "1TTRAKMR";
 
-/*
-//retreive friends for current user
-console.log("before function");
-//let friends: any = getFriends(primaryUser);
-//console.log("after function")
-//console.log(friends)
-//let friendsList = JSON.parse(getFriends(primaryUser));
 
-getFriends(primaryUser).then((friends) =>{
-	let friendsList: string[] = friends;
+
+//let friendsList: string[] = ["8C0IXA8C","9UD2ZWJT","F0UF7G5E","H982UZHS","ISCVQN7A",]
+//console.log(friendsList.length);  
+
+async function getFriends(userid: string){
+	const response = await fetch(endpoint + "/getFriends", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      userid: userid,
+    }
+  })
+    const json = await response.json();
+  
+  return json;
+	
+}
+
+async function something() {
+    const friendsList = await getFriends(primaryUser);
+    //console.log(friendsList.friends);
+	return friendsList.friends;
+}
+
+const [data,setData]= useState([]);
+  
+let promise = something();
+
+promise.then(result => {
+    setData(result)
 });
-*/
+console.log(data);
 
-let friendsList: string[] = ["8C0IXA8C","9UD2ZWJT","F0UF7G5E","H982UZHS","ISCVQN7A",]
-console.log(friendsList.length);  
-
- 
 
 export default function FriendsScreen() {
+ 
+
   return (
     <View style={styles.container}>
 	   
 	  <TouchableOpacity
-         onPress={() => getFriends()}
+         
 		 style={styles.button1}>
-         <Text style={styles.buttonText}>{friendsList[0]}</Text>
+         <Text style={styles.buttonText}>Hello</Text>
       </TouchableOpacity>
 	  
-	  <TouchableOpacity
-         onPress={() => getFriends()}
-		 style={styles.button2}>
-         <Text style={styles.buttonText}>{friendsList[1]}</Text>
-      </TouchableOpacity>
 	  
-	  <TouchableOpacity
-         onPress={() => getFriends()}
-		 style={styles.button3}>
-         <Text style={styles.buttonText}>{friendsList[2]}</Text>
-      </TouchableOpacity>
-    
-	
-	<TouchableOpacity
-         onPress={() => getFriends()}
-		 style={styles.button4}>
-         <Text style={styles.buttonText}>{friendsList[3]}</Text>
-      </TouchableOpacity>
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
